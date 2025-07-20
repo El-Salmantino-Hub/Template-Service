390948671b25a8691fa0209714831d55e94e9f6a
Jorge Mora <jorgemorah671@gmail.com>
2025-07-20 11:57:01 -0600

    feat: restructure API routing and add dependent services check

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
 import { config } from '@config/env';
 import { defineMiddleware } from '@api/middlewares';
-import { defineRouter } from '@api/router';
+import { defineRouter } from '@/api/routes';
+import { DependentServicesService } from '@api/services';
 import { logger } from '@utils/logger';
 import { setReady } from '@states/index';
 
@@ -16,6 +17,8 @@ export async function initializeServer() {
   app.listen(PORT, () => {
     logger.info(`Server is running on http://localhost:${PORT}`);
   }); 
+
+  await DependentServicesService.checkDependentServices();
   
   setReady()
   logger.debug('Server is ready and listening for requests')
